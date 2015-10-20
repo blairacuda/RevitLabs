d2f85b41eba8edb7aa340186d50a2c356943b06e
Chris Blair <t.chris.blair@gmail.com>
2015-10-20 16:36:35 -0700

    lab1 complete
    added external command to display simple data

--- RevitLabs/Lab1.cs
+++ RevitLabs/Lab1.cs
@@ -19,6 +19,37 @@ namespace RevitLabs
         }
     }
 
+
+    [Transaction(TransactionMode.ReadOnly)]
+    public class CommandData : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var rvtUiApp = commandData.Application;
+            var rvtApp = rvtUiApp.Application;
+            var rvtUiDoc = rvtUiApp.ActiveUIDocument;
+            var rvtDoc = rvtUiDoc.Document;
+
+            var versionName = rvtApp.VersionName;
+            var documentTitle = rvtDoc.Title;
+
+            TaskDialog.Show( "Revit Intro Lab", "Version Name = " + versionName + "\nDocument Title = " + documentTitle);
+
+            var collector = new FilteredElementCollector(rvtDoc);
+            collector.OfClass(typeof(WallType));
+            var s = "";
+            foreach (WallType wallType in collector)
+            {
+                s += wallType.Name + "\r\n";
+            }
+
+            TaskDialog.Show( "Revit Intro Lab", "Wall Types (in main instruction):\n\n" + s);
+
+
+            return Result.Succeeded;
+        }
+    }
+
     /// <summary>
     /// A minimum Revit external application. 
     /// </summary>
@@ -33,7 +64,7 @@ namespace RevitLabs
         // OnStartup() - called when Revit starts. 
         public Result OnStartup( UIControlledApplication application )
         {
-            TaskDialog.Show( "My Dialog Title", "Hello World from App!" );
+            //TaskDialog.Show( "My Dialog Title", "Hello World from App!" );
             return Result.Succeeded;
         }
     }
