fefbd2cdf9dcd310a0b7e3a4e48844bc36775603
Chris Blair <t.chris.blair@gmail.com>
2015-10-20 16:01:06 -0700

    Lab1 External Command
    external command from lab1.  added build event to copy the .adding to
    the proper location and changeg debug to launch revit

---
+++ RevitLabs/Lab1.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+
+namespace RevitLabs
+{
+    /// <summary>
+    /// Hello World #1 - A minimum Revit external command. 
+    /// </summary>
+    [Transaction(Autodesk.Revit.Attributes.TransactionMode.ReadOnly)]
+    public class HelloWorld : IExternalCommand
+    {
+        public Result Execute( ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show( "My Dialog Title", "Hello World!");
+
+            return Result.Succeeded;
+        }
+    }
+
+}
